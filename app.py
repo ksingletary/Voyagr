a8cc4fcae69669ef24d9568ebb92c641d51cce17
Keith Kodes <code.keith@Keiths-MBP.lan>
2024-01-04 17:28:53 -0500

    canceling trip functionality & error-handling/valildation

--- app.py
+++ app.py
@@ -106,8 +106,7 @@ def destinations_page():
 #Individual Destinations
 
 global booked_package
-booked_package = set()          #set so there's no duplicate bookings
-canceled_package = set()        #recording of canceled trip for user
+booked_package = []          #set so there's no duplicate bookings
 
 @app.route('/voyagr/tokyo', methods=["GET", "POST"])
 @require_login
@@ -116,19 +115,19 @@ def destinations_tokyo():
 
     location = Location.query.get_or_404(1)
     trip = Trip.query.get_or_404(1)            
-    
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form.validate_on_submit():
-        if form.booking.data:
-            if location.city in booked_package:         #if city is already in booked_package 
-                print("Already in booked_package so we print this!")
-            else:
-                booked_package.add(location.city)
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} trip! View in your profile")
+    if form.validate_on_submit() and form.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form.validate_on_submit() and form.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city)
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} trip! View in your profile")
 
         return redirect('/voyagr/tokyo')
     
@@ -144,16 +143,17 @@ def destinations_tokyo_p():
 
     form = BookedTrips()
     form1 = PremiumBookedTrips()
-    if form1.validate_on_submit():
-        if form1.premium_booking.data:
-            if location.city in booked_package:
-                print("Already in booked_package so we print this!")
-            else:
-                booked_package.add(f"{location.city}P")
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
+    if form1.validate_on_submit() and form1.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form1.validate_on_submit() and form1.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city + 'P')
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
         
         return redirect('/voyagr/tokyo')
     
@@ -169,16 +169,17 @@ def destinations_rome():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form.validate_on_submit():
-        if form.booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(location.city)
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} trip! View in your profile")
+    if form.validate_on_submit() and form.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form.validate_on_submit() and form.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city)
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} trip! View in your profile")
 
         return redirect('/voyagr/rome')
 
@@ -194,16 +195,17 @@ def destinations_rome_p():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form1.validate_on_submit():
-        if form1.premium_booking.data:
-            if location.city in booked_package:
-                flash("Already booked!")
-            else:
-                booked_package.add(f"{location.city}P")
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
+    if form1.validate_on_submit() and form1.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form1.validate_on_submit() and form1.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city + 'P')
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
 
         return redirect('/voyagr/rome')
 
@@ -219,16 +221,17 @@ def destinations_giza():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form.validate_on_submit():
-        if form.booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(location.city)
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} trip! View in your profile")
+    if form.validate_on_submit() and form.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form.validate_on_submit() and form.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city)
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} trip! View in your profile")
 
         return redirect('/voyagr/giza')
 
@@ -245,16 +248,17 @@ def destinations_giza_p():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form1.validate_on_submit():
-        if form1.premium_booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(f"{location.city}P")
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
+    if form1.validate_on_submit() and form1.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form1.validate_on_submit() and form1.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city + 'P')
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
 
         return redirect('/voyagr/giza')
 
@@ -271,16 +275,17 @@ def destinations_paris():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form.validate_on_submit():
-        if form.booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(location.city)
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} trip! View in your profile")
+    if form.validate_on_submit() and form.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form.validate_on_submit() and form.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city)
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} trip! View in your profile")
 
         return redirect('/voyagr/paris')
 
@@ -297,16 +302,17 @@ def destinations_paris_p():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form1.validate_on_submit():
-        if form1.premium_booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(f"{location.city}P")
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
+    if form1.validate_on_submit() and form1.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form1.validate_on_submit() and form1.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city + 'P')
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
 
         return redirect('/voyagr/paris')
 
@@ -323,17 +329,17 @@ def destinations_dubai():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form.validate_on_submit():
-        if form.booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(location.city)
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} trip! View in your profile")
-
+    if form.validate_on_submit() and form.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form.validate_on_submit() and form.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city)
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} trip! View in your profile")
         return redirect('/voyagr/dubai')
 
     return render_template('dubai.html', form=form, form1=form1)
@@ -348,17 +354,17 @@ def destinations_dubai_p():
     
     form1 = PremiumBookedTrips()
     form = BookedTrips()
-    if form1.validate_on_submit():
-        if form1.premium_booking.data:
-            if location.city in booked_package:
-                print("Already booked!")
-            else:
-                booked_package.add(f"{location.city}P")
-                booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
-                db.session.add(booked_trip)
-                db.session.commit()
-                flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
-
+    if form1.validate_on_submit() and form1.date.data == '1':
+        flash("You must choose a date for your trip!")
+    if form1.validate_on_submit() and form1.date.data != '1':
+        if location.city in booked_package or (location.city + 'P') in booked_package:         #if city is already in booked_package 
+            flash(f"Already booked your {location.country} trip!")
+        else:
+            booked_package.append(location.city + 'P')
+            booked_trip = Booked_Trip(cost=trip.cost, location_id=location.id, user_id=session['username'])
+            db.session.add(booked_trip)
+            db.session.commit()
+            flash(f"Succesfully booked your {location.country} premium trip! View in your profile")
         return redirect('/voyagr/dubai')
 
     return render_template('dubai.html', form=form, form1=form1)
@@ -367,28 +373,34 @@ def destinations_dubai_p():
 @require_login
 def user_show(user_id):
     """Show User Profile"""
-
+    form = CancelTrip()
     user = User.query.get_or_404(user_id)
-    if booked_package:
-        booked_trips = Booked_Trip.query.filter(Booked_Trip.user_id == user.id).all()           #call booked_trips for respective user id bookings
-        new_booked_packaged = list(booked_package)
+    trip = Trip.query.filter(Trip.location_id == Booked_Trip.location_id).all()
 
-        # call trip where location id is equal to bookedtrip location id            
-        trip = Trip.query.filter(Trip.location_id == Booked_Trip.location_id).all()
-        activities = Activity.query.all()
-        form = CancelTrip()
+    if user.id != session['username']:
+        return redirect(f"/voyagr/users/{session['username']}")
+
+    if booked_package:
+        booked_trips = Booked_Trip.query.filter_by(user_id=user.id).all()
+
         if form.validate_on_submit():
-            canceled_package.add("Trip[x] Canceled")
-            flash("We have successfully canceled your trip! It may still appear in your profile, but it has been recorded!")
+            trip_to_cancel = form.cancel.data  # Assuming the cancellation data is the trip ID
+            for booked_trip in range(len(booked_package)):
+                if booked_package[booked_trip] == trip_to_cancel or booked_package[booked_trip][0:-1] == form.cancel.data :
+                    booked_package.remove(booked_package[booked_trip])
+                    db.session.delete(booked_trips[booked_trip])
+                    db.session.commit()
+                    flash("Successfully canceled the trip!")
+                    return redirect(f'/voyagr/users/{user.id}')
+            flash("Trip ID not found in your bookings. No trip canceled.")
             return redirect(f'/voyagr/users/{user.id}')
-        
-        return render_template('user.html', user=user, trip=trip, booked_package=booked_package, booked_trips=booked_trips, activities=activities, new_booked_packaged=new_booked_packaged, form=form)
+        return render_template('user.html', user=user, trip=trip, booked_package=booked_package, booked_trips=booked_trips, form=form)
     
     if user.id != session['username']:
         return redirect(f"/voyagr/users/{session['username']}")
 
     
-    return render_template('user.html', user=user, booked_package=booked_package)
+    return render_template('user.html', user=user, booked_package=booked_package, form=form)
 
 
 @app.route('/voyagr/users/<int:user_id>/edit', methods=["GET", "POST"])
--- forms.py
+++ forms.py
@@ -19,19 +19,20 @@ class EditProfileForm(FlaskForm):
 class BookedTrips(FlaskForm):
     """Booking Trips Button"""
 
-    date = SelectField("Available Dates", choices=[(1, 'Available Dates'), (2, '03/5/24 - 03/12/24'), (3, '04/10/24 - 04/17/24'), (4, '05/1/24 - 05/8/24'), (5, '01/5/24 - 01/12/24'), (6, '02/10/24 - 02/17/24'), (7, '03/1/24 - 03/8/24'), (8, '06/5/24 - 06/12/24'), (9, '07/10/24 - 07/17/24'), (10, '08/1/24 - 08/8/24')])
+    date = SelectField("Available Dates", validators=[InputRequired()], choices=[(1, 'Available Dates'), (2, '03/5/24 - 03/12/24'), (3, '04/10/24 - 04/17/24'), (4, '05/1/24 - 05/8/24'), (5, '01/5/24 - 01/12/24'), (6, '02/10/24 - 02/17/24'), (7, '03/1/24 - 03/8/24'), (8, '06/5/24 - 06/12/24'), (9, '07/10/24 - 07/17/24'), (10, '08/1/24 - 08/8/24')])
     booking = SubmitField("Book Now")
 
 class PremiumBookedTrips(FlaskForm):
     """Booking Trips Button"""
 
-    date = SelectField("Available Dates", choices=[(1, 'Available Dates'), (2, '03/5/24 - 03/19/24'), (3, '04/10/24 - 04/24/24'), (4, '05/1/24 - 05/15/24'), (5, '01/5/24 - 01/19/24'), (6, '02/10/24 - 02/24/24'), (7, '03/1/24 - 03/15/24'), (8, '06/5/24 - 06/19/24'), (9, '07/10/24 - 07/24/24'), (10, '08/1/24 - 08/15/24')])
+    date = SelectField("Available Dates", validators=[InputRequired()], choices=[(1, 'Available Dates'), (2, '03/5/24 - 03/19/24'), (3, '04/10/24 - 04/24/24'), (4, '05/1/24 - 05/15/24'), (5, '01/5/24 - 01/19/24'), (6, '02/10/24 - 02/24/24'), (7, '03/1/24 - 03/15/24'), (8, '06/5/24 - 06/19/24'), (9, '07/10/24 - 07/24/24'), (10, '08/1/24 - 08/15/24')])
     premium_booking = SubmitField("Book Now")
 
 class CancelTrip(FlaskForm):
     """Canceling a trip"""
 
-    cancel = SubmitField("Cancel Trip")
+    cancel = SelectField("Cancel Trip", choices=[("Tokyo", "Tokyo"), ("Rome", "Rome"), ("Greater Cairo", "Greater Cairo"), ("Dubai", "Dubai"), ("Paris", "Paris") ])
+    submit_cancellation = SubmitField("Cancel Trip")
 
 
 
